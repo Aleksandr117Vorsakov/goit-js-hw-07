18a3d1b36870d3e5456ea67a76d3868cab2a7f77
Aleksandr117Vorsakov <vorsakov1994@gmail.com>
2022-08-05 18:06:21 +0300

    галерея -1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,56 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+const galleryRefs = document.querySelector(".gallery");
+galleryRefs.insertAdjacentHTML('beforeend',createGalleryMarkup(galleryItems));
+galleryRefs.addEventListener('click', onGalleryItemClick);
+let  instance;
 
-console.log(galleryItems);
+function createGalleryMarkup ( images){
+    return images
+      .map(({preview, original, description}) =>
+        `<div class="gallery__item">
+          <a class="gallery__link" href="${original}">
+             <img
+             class="gallery__image"
+             src="${preview}"
+             data-source="${original}"
+             alt="${description}"
+             />
+         </a>
+        </div>
+        `)
+        .join('');
+
+}
+
+function onGalleryItemClick(evt){
+    evt.preventDefault();
+    if(evt.target.nodeName !== "IMG"){
+        return
+    }
+    onOpenModal(evt);
+}
+
+function onOpenModal(evt){
+    instance = basicLightbox.create(
+        `<img src="${evt.target.dataset.source}">`,
+        {
+          onShow: (instance) => {
+            window.addEventListener("keydown", onEscKeyPress);
+          },
+          onClose: (instance) => {
+            window.removeEventListener("keydown", onEscKeyPress);
+          },
+        }
+      );
+      instance.show();
+    }
+
+function onCloseModal() {
+    instance.close();
+}
+function onEscKeyPress(evt) {
+    if (evt.code === "Escape") {
+        onCloseModal();
+    }
+  }
